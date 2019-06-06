ebf3e17abb107418c2d7496458e75a847eef3191
jsyum <jennifersyum@gmail.com>
2019-06-06 00:40:49 -0400

    working on mapping images src

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from "react";
 import "./App.css";
+import Images from "./components/Images";
+
+class ShowImage extends React.Component {
+  render() {
+    return (
+      <div className="Images">
+        {this.state.images.map(image => {
+          return <img src="{images.src}" />;
+        })}
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div>
       <h1>Clicky Game</h1>
+      <h2>
+        Click on an image to earn points, but don't click on any more than once
+      </h2>
     </div>
   );
 }
 
-export default App;
+export default ShowImage;
---
+++ src/components/Images.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Images extends React.Component {
+  images = [
+    { id: 1, src: "../../public/img/chapel.jpg" },
+    { id: 2, src: "../../public/img/martinback.jpg" },
+    { id: 3, src: "../../public/img/martinbuds.jpg" }
+  ];
+}
+
+export default Images;
