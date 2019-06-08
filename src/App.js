a120fbf00442a598d0fe75f9cd3b8e879219689b
jsyum <jennifersyum@gmail.com>
2019-06-08 00:33:12 -0400

    map img progress

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
 import React from "react";
 import "./App.css";
-import Images from "./components/Images";
+// import Images from "./components/Images";
 
-class ShowImage extends React.Component {
+<ImageRow>
+  {images.map(img => (
+    <Image src={img.src} />
+  ))}
+</ImageRow>;
+
+// class ShowImage extends React.Component {
+//   render() {
+//     return (
+//       <div className="Images">
+//         {this.state.images.map(image => {
+//           return <img src="{images.src}" />;
+//         })}
+//       </div>
+//     );
+//   }
+// }
+
+class ImageRow extends React.Component {
+  state = {
+    images: [
+      { id: 1, src: "../../public/img/chapel.jpg", wasClicked: false },
+      { id: 2, src: "../../public/img/martinback.jpg", wasClicked: false },
+      { id: 3, src: "../../public/img/martinbuds.jpg", wasClicked: false }
+    ]
+  };
+
+  componentDidMount() {
+    axios.get("/api/images").then(images => this.setState(images));
+  }
+
+  onImgClick(idx) {
+    // update the wasClicked status of the clicked image
+    newImages = [...this.state.images];
+    newImages[idx].wasClicked == true;
+    this.setState({ images: newImages });
+  }
+
   render() {
     return (
-      <div className="Images">
-        {this.state.images.map(image => {
-          return <img src="{images.src}" />;
-        })}
+      <div>
+        {this.state.images.map(img, idx => (
+          <Image
+            key={img.id}
+            src={img.src}
+            wasClicked={img.wasClicked}
+            onClick={() => this.onImgClick(idx)}
+          />
+        ))}
       </div>
     );
   }
@@ -26,3 +68,4 @@ function App() {
 }
 
 export default ShowImage;
+export const test = "test";
--- src/components/Images.js
+++ src/components/Images.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 class Images extends React.Component {
-  images = [
-    { id: 1, src: "../../public/img/chapel.jpg" },
-    { id: 2, src: "../../public/img/martinback.jpg" },
-    { id: 3, src: "../../public/img/martinbuds.jpg" }
-  ];
+  
 }
 
 export default Images;
