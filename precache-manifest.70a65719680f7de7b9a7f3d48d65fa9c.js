fc9d4b4da4a2db37441093d4ca26f428e02cf186
jsyum <jennifersyum@gmail.com>
2019-06-11 00:15:38 -0400

    Updates

---
+++ precache-manifest.70a65719680f7de7b9a7f3d48d65fa9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af7257d268587180d5c05ba65fd144d7",
+    "url": "/memorygame/index.html"
+  },
+  {
+    "revision": "713e9adc8efa810e59f1",
+    "url": "/memorygame/static/css/main.e5756fde.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/memorygame/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "713e9adc8efa810e59f1",
+    "url": "/memorygame/static/js/main.bd91a94d.chunk.js"
+  },
+  {
+    "revision": "b62309af7e76175547d6",
+    "url": "/memorygame/static/js/runtime~main.d170bb03.js"
+  }
+]);
