19a90ac9a0618f4929938a590a80493cd4f52452
jsyum <jennifersyum@gmail.com>
2019-06-10 20:29:48 -0400

    refactored img

--- src/components/Images/Images.js
+++ src/components/Images/Images.js
@@ -1,28 +1,76 @@
 import React, { Component } from "react";
 import "./Images.css";
 
-class Images extends Component {
+class App extends Component {
   state = {
-    sources: [
-      "https://i.ibb.co/PZrpHPg/chapel.jpg",
-      "https://i.ibb.co/w0KFQRF/martinback.jpg",
-      "https://i.ibb.co/dj7Kmt7/martingratitude.jpg",
-      "https://i.ibb.co/xJS5177/martinbuds.jpg"
-    ],
-    clicked: []
+    paintings: [
+      {
+        id: 1,
+        name: "rothko chapel",
+        src: "https://i.ibb.co/PZrpHPg/chapel.jpg",
+        clicked: false
+      },
+      {
+        id: 2,
+        name: "with my back to the world",
+        src: "https://i.ibb.co/w0KFQRF/martinback.jpg",
+        clicked: false
+      },
+      {
+        id: 3,
+        name: "gratitude",
+        src: "https://i.ibb.co/dj7Kmt7/martingratitude.jpg",
+        clicked: false
+      },
+      {
+        id: 4,
+        name: "buds",
+        src: "https://i.ibb.co/xJS5177/martinbuds.jpg",
+        clicked: false
+      },
+      {
+        id: 5,
+        name: "faraway love",
+        src: "https://i.ibb.co/QHP1xMd/martinlove.jpg",
+        clicked: false
+      },
+      {
+        id: 6,
+        name: "morning",
+        src: "https://i.ibb.co/PzKTjwL/martinmorning.jpg",
+        clicked: false
+      },
+      {
+        id: 7,
+        name: "rothko grey",
+        src: "https://i.ibb.co/4PYM0xR/rothkogrey.jpg",
+        clicked: false
+      },
+      {
+        id: 8,
+        name: "four darks in red",
+        src: "https://i.ibb.co/V3DCgB8/rothkored.jpg",
+        clicked: false
+      },
+      {
+        id: 9,
+        name: "rust and blue",
+        src: "https://i.ibb.co/1J7Rb1H/rothkorust.jpg",
+        clicked: false
+      }
+    ]
   };
 
   renderImg() {
     return (
       <div>
-        {this.state.sources.map(source => (
+        {this.state.paintings.map(source => (
           <img
             width="200px"
             height="200px"
-            className="galleryImg"
-            src={source}
-            key={source}
-            alt="blob"
+            src={source.src}
+            key={source.id}
+            alt={source.name}
           />
         ))}
       </div>
@@ -33,4 +81,4 @@ class Images extends Component {
     return <div>{this.renderImg()}</div>;
   }
 }
-export default Images;
+export default App;
